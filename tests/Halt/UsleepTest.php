ef12979d14789524f46abce3d3d15ec590c39fb4
Baptiste Langlade <baptouuuu@gmail.com>
2018-09-23 12:38:19 +0200

    add usleep implementation to halt program

---
+++ tests/Halt/UsleepTest.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\TimeWarp\Halt;
+
+use Innmind\TimeWarp\{
+    Halt\Usleep,
+    Halt,
+};
+use Innmind\TimeContinuum\{
+    TimeContinuumInterface,
+    Period\Earth\Millisecond,
+};
+use PHPUnit\Framework\TestCase;
+
+class UsleepTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(Halt::class, new Usleep);
+    }
+
+    public function testHalt()
+    {
+        $sleep = new Usleep;
+
+        $start = microtime(true);
+        $this->assertNull($sleep(
+            $this->createMock(TimeContinuumInterface::class),
+            new Millisecond(500)
+        ));
+        $end = microtime(true);
+        $this->assertEquals(0.5, $end - $start, '', 0.01); // 10 milliseconds delta allowed
+    }
+}
